b6b820ec9c06791d9beac451b2fc2c0d781af296
laibaIdrees <122279127+LaibaIdreees@users.noreply.github.com>
2023-12-05 21:24:55 +0500

    First

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def Add(a, b):
         return a + b
         
 def SayHello():
-        print("DevOps Live Demo")
+        print("DevOps Live Demo CircleCI")
 
 if __name__ == '__main__':
         SayHello()
